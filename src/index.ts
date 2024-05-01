61bb9b41f60c4b08e104e4ec26f2742cd099d24c
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-05-01 14:14:38 -0400

    add comment body trim

--- src/index.ts
+++ src/index.ts
@@ -5,62 +5,62 @@ import type { GH_Response } from "./types.js";
 import { createCoauthorString } from "./utils.js";
 
 async function run() {
-  const triggerPhrase = core.getInput("trigger-phrase");
+	const triggerPhrase = core.getInput("trigger-phrase");
+	const commentBody = github.context.payload.comment?.body;
+	// Check if the comment contains the trigger word
+	if (commentBody.trim() !== triggerPhrase) {
+		core.notice(`Skipping, comment does not contain '${triggerPhrase}'`);
+		return;
+	}
 
-  // Check if the comment contains the trigger word
-  if (github.context.payload.comment?.body !== triggerPhrase) {
-    core.notice(`Skipping, comment does not contain '${triggerPhrase}'`);
-    return;
-  }
+	// Get context data
+	const token = core.getInput("token", { required: true });
+	const octokit = github.getOctokit(token);
+	const pr = github.context.payload.issue?.number;
+	const author = github.context.payload.issue?.user.login;
+	const { owner, repo } = github.context.repo;
 
-  // Get context data
-  const token = core.getInput("token", { required: true });
-  const octokit = github.getOctokit(token);
-  const pr = github.context.payload.issue?.number;
-  const author = github.context.payload.issue?.user.login;
-  const { owner, repo } = github.context.repo;
+	// Check for required context data
+	if (!pr || !author || !owner || !repo) {
+		core.setFailed("Could not get pull request number, owner or repo");
+		return;
+	}
 
-  // Check for required context data
-  if (!pr || !author || !owner || !repo) {
-    core.setFailed("Could not get pull request number, owner or repo");
-    return;
-  }
+	try {
+		const data: GH_Response = await octokit.graphql(query, {
+			owner,
+			repo,
+			pr,
+		});
 
-  try {
-    const data: GH_Response = await octokit.graphql(query, {
-      owner,
-      repo,
-      pr,
-    });
+		const participants = (data.repository.pullRequest.participants.nodes ?? [])
+			.map(({ name, login, databaseId, email }) => ({
+				name,
+				login,
+				id: databaseId,
+				email,
+			}))
+			// remove the author from the list of participants
+			.filter((p) => p.login !== author);
 
-    const participants = (data.repository.pullRequest.participants.nodes ?? [])
-      .map(({ name, login, databaseId, email }) => ({
-        name,
-        login,
-        id: databaseId,
-        email,
-      }))
-      // remove the author from the list of participants
-      .filter((p) => p.login !== author);
-
-    if (participants.length === 0) {
-      core.notice("No co-authors found");
-      return;
-    }
-    core.info(`Found ${participants.length} co-authors`);
-    const coauthorString = participants.map(createCoauthorString).join("\n");
-    core.info("Created coauthor string");
-    const commentBody = `\`\`\`\n${coauthorString}\n\`\`\``;
-    const { data: comment } = await octokit.rest.issues.createComment({
-      owner,
-      repo,
-      issue_number: pr,
-      body: commentBody,
-    });
-    core.info(`Created comment id '${comment.id}' on pull request '${pr}'.`);
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message);
-  }
+		if (participants.length === 0) {
+			core.notice("No co-authors found");
+			return;
+		}
+		core.info(`Found ${participants.length} co-authors`);
+		const coauthorString = participants.map(createCoauthorString).join("\n");
+		core.info("Created coauthor string");
+		const commentBody = `\`\`\`\n${coauthorString}\n\`\`\``;
+		const { data: comment } = await octokit.rest.issues.createComment({
+			owner,
+			repo,
+			issue_number: pr,
+			body: commentBody,
+		});
+		core.info(`Created comment id '${comment.id}' on pull request '${pr}'.`);
+	} catch (error) {
+		if (error instanceof Error) core.setFailed(error.message);
+	}
 }
 
 run();
